95d35a22e4bb19871f65bb6a5fe485fd5e93179b
kjm541006 <kjm541113@gmail.com>
2022-09-16 22:50:40 +0900

    장바구니 추가 버튼 스타일링

--- src/components/Foods/FoodItem/FoodItemForm.js
+++ src/components/Foods/FoodItem/FoodItemForm.js
@@ -1,9 +1,9 @@
 import React from "react";
-import classes from "./FoodItem.module.css";
+import classes from "./FoodItemForm.module.css";
 
 export default function FoodItemForm(props) {
   return (
-    <form>
+    <form className={classes.form}>
       <input />
       <button>장바구니에 추가</button>
     </form>
