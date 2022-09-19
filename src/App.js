014969248b2b6e673ea781ebc0274ed80d80fd7f
kjm541006 <kjm541113@gmail.com>
2022-09-19 23:33:48 +0900

    장바구니 Modal 및 portal 구현 및 디자인

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import React from "react";
 import Header from "./components/Layout/Header";
 import Foods from "./components/Foods/Foods";
+import Cart from "./components/Cart/Cart";
 function App() {
   return (
-    <>
+    <React.Fragment>
+      <Cart />
       <Header />
       <main>
         <Foods />
       </main>
-    </>
+    </React.Fragment>
   );
 }
 
